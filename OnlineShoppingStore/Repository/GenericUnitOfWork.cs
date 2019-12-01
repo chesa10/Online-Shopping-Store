27475be7337f69280ab44b013e59154390b94a3c
abram chesa mmako <brmmalesela@gmail.com>
2019-12-01 13:28:11 +0200

    Added Repository Pattern

---
+++ OnlineShoppingStore/Repository/GenericUnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using OnlineShoppingStore.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore.Repository
+{
+    public class GenericUnitOfWork: IDisposable
+    {
+        private dbMyOnlineShoppingEntities DBEntity = new dbMyOnlineShoppingEntities();
+        public IRepository<Tbl_EntityType> GetRepositoryInstance<Tbl_EntityType>() where Tbl_EntityType: class
+        {
+            return new GenericReposity<Tbl_EntityType>(DBEntity);
+        }
+
+        public void SaveChanges()
+        {
+            DBEntity.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    DBEntity.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        private bool disposed = false;
+    }
+}
