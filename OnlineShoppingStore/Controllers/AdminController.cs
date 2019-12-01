90da076aef1c55858f228797ba94ad150a95bb8d
abram chesa mmako <brmmalesela@gmail.com>
2019-12-02 00:36:33 +0200

    Added Category.

---
+++ OnlineShoppingStore/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using OnlineShoppingStore.DataAccessLayer;
+using OnlineShoppingStore.Models;
+using OnlineShoppingStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        public GenericUnitOfWork _genericUnitOfWork = new GenericUnitOfWork();
+
+        // GET: Admin
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allcategories = 
+                _genericUnitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+
+            return View(allcategories);
+        }
+
+        public ActionResult AddCategory()
+        {
+            return UpdateCategory(0);
+        }
+
+        public ActionResult UpdateCategory(int categoryId)
+        {
+            CategoryDetail cd;
+            if (categoryId != null)
+            {
+                cd = JsonConvert.DeserializeObject<CategoryDetail>(JsonConvert.SerializeObject(_genericUnitOfWork.GetRepositoryInstance<Tbl_Category>().GetFirstorDefault(categoryId)));
+            }
+            else
+            {
+                cd = new CategoryDetail();
+            }
+            return View("UpdateCategory", cd);
+        }
+    }
+}
